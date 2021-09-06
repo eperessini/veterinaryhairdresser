43c2e1f0c8d094f0f65ad294f335c629f022041f
Emiliano Peressini <emiliano.peressini@gmail.com>
2021-09-06 11:54:00 -0300

    Se agrego dependencia Swagger

--- src/main/java/ar/edu/undec/veterinaryhairdresser/VeterinaryhairdresserApplication.java
+++ src/main/java/ar/edu/undec/veterinaryhairdresser/VeterinaryhairdresserApplication.java
@@ -2,8 +2,10 @@ package ar.edu.undec.veterinaryhairdresser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class VeterinaryhairdresserApplication {
 
     public static void main(String[] args) {
